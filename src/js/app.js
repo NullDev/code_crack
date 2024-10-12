96df93d30179a9bf4da89a5d1757dbaf0ca19a1f
NullDev (Shadow) <chris@nulldev.org>
2024-10-12 23:33:45 +0200

    add footer section

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,26 @@ import initPinContainers from "./util/initPinContainers.js";
 import solver from "./solve/solver.js";
 
 /**
+ * Set the current year in the footer
+ *
+ * @return {void}
+ */
+const copyrightYear = function(){
+    const yearElement = /** @type {HTMLSpanElement} */ (document.getElementById("y"));
+    if (!yearElement) return;
+
+    const currentYear = new Date().getFullYear();
+    yearElement.innerText = String(currentYear);
+};
+
+/**
  * Solve the puzzle
  *
- * @returns {void}
+ * @return {void}
  */
 const solve = function(){
     const resultElement = /** @type {HTMLSpanElement} */ (document.getElementById("result"));
-    if (resultElement === null) return;
+    if (!resultElement) return;
 
     const result = solver();
     resultElement.innerHTML = result;
@@ -18,6 +31,7 @@ const solve = function(){
 (() => {
     document.addEventListener("DOMContentLoaded", function(){
         initPinContainers();
+        copyrightYear();
         document.getElementById("crack")?.addEventListener("click", solve);
     });
 })();
